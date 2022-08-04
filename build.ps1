6946de70ec09e004e1386027d77763c193e43ba6
thautwarm <twshere@outlook.com>
2022-08-05 07:29:40 +0800

    pypi package

--- build.ps1
+++ build.ps1
@@ -1,11 +1,2 @@
-fable-py --outDir fable_sedlex
-
-
-# https://github.com/fable-compiler/Fable.Python/issues/30#issuecomment-1002059876
-"import sys;import os;sys.path.append(os.path.dirname(__file__))" | Out-File -FilePath "./fable_sedlex/__init__.py" 
-
-"" | Out-File -FilePath "./fable_sedlex/fable_modules/__init__.py" 
-
-try
-{ rm fable_sedlex/fable_modules/.gitignore -ErrorAction Stop }
-catch { }
+fable --lang Python Fable.Sedlex.fsproj -o fable_sedlex
+echo > fable_sedlex/fable_modules/__init__.py
